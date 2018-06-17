8609aa6625d9ad0c010f4c5cdb0702a4bb9f818b
hohh0115 <hohh0115@gmail.com>
2018-06-17 15:46:47 +0800

    "delete and do some fixes"

--- Singly Linked List/build_insert_traverse_01.py
+++ Singly Linked List/build_insert_traverse_01.py
@@ -8,8 +8,8 @@ class Node():
         self.data = initdata
         self.next = None
 
-def do_list_traverse():
-    ptr = head.next
+def do_list_traverse(node_head):
+    ptr = node_head.next
     while ptr != None:
         print(ptr.data)
         ptr = ptr.next
@@ -48,65 +48,72 @@ def insert_at_head(head, insert_count, loop_count):
         new_data.next = head.next
         head.next = new_data
 
-def insert_at_point(begin_node, insert_count, insert_index, loop_count):
+def insert_at_point(begin_node, insert_count, insert_at, loop_count):
     """
     插入任意位置
     :param begin_node: linked list第幾個位置的前一個節點
     :param insert_count: 插入多少節點
-    :param insert_index: 插入第幾個位置
+    :param insert_at: 插入第幾個位置
     :param loop_count: 使用者操作次數
     :return:
     """
     for i in range(1, insert_count + 1):
-        new_data = Node(str(randint(1, 500)) + '-插入第' + str(insert_index) + '位置No.' + str(loop_count) + '-' + str(i))
+        new_data = Node(str(randint(1, 500)) + '-插入第' + str(insert_at) + '位置No.' + str(loop_count) + '-' + str(i))
         new_data.next = begin_node.next
         begin_node.next = new_data
 
-make_list = False
-select = 0 # 使用者選擇哪個方法
-count = 0 # 使用者選擇產生幾個節點
-head = Node() # linked list的首指標
-insert_point = 0 # 第幾個位置做插入
-loop_count = 1 # 使用者操作幾次
+if __name__ == "__main__":
 
-while make_list is not True:
-    print('使用尾插法:1\n使用頭插法：2\n任意插入：3\n離開：-1')
-    try:
-        select = int(input('輸入一個選項:'))
-        if select > 0:
-            count = int(input('要有幾個節點?'))
-    except ValueError:
-        print('輸入錯誤，請重新輸入！')
-        continue
+    make_list = True
+    select = 0 # 使用者選擇哪個方法
+    count = 0 # 使用者選擇產生幾個節點
+    head = Node() # linked list的首指標
+    insert_point = 0 # 第幾個位置做插入
+    loop_count = 1 # 使用者操作幾次
 
-    if select == 1:
-        begin_node = refresh_to_end_node(head)
-        insert_at_end(begin_node, count, loop_count)
-    elif select == 2:
-        insert_at_head(head, count, loop_count)
-    elif select == 3:
-
-        insert_index = int(input('從第幾個位置開始插入?'))
-        j = 1
-        point = head
+    while make_list is not False:
+        print('使用尾插法:1\n使用頭插法：2\n任意插入：3\n離開：-1')
         try:
-            while j < insert_index: # 第insert_index位置的前一個節點就可以，第insert_index位置的節點用其前一個節點的指標做指向
-                point = point.next
-                j += 1
-        except AttributeError:
-            print('該位置不存在')
+            select = int(input('輸入一個選項:'))
+            if select > 0:
+                count = int(input('要有幾個節點?'))
+        except ValueError:
+            print('輸入錯誤，請重新輸入！')
             continue
 
-        insert_at_point(point, count, insert_index, loop_count)
-    elif select == -1:
-        make_list = True
-    else:
-        print('看不懂')
-        continue
+        if select == 1:
+            begin_node = refresh_to_end_node(head)
+            insert_at_end(begin_node, count, loop_count)
+        elif select == 2:
+            insert_at_head(head, count, loop_count)
+        elif select == 3:
 
-    loop_count += 1
-    do_list_traverse()
-    print('========================')
+            insert_at = int(input('從第幾個位置開始插入?'))
+            j = 1
+            point = head
+            try:
+                while j < insert_at: # 第insert_at位置的前一個節點就可以，第insert_at位置的節點用其前一個節點的指標做指向
+                    point = point.next
+                    j += 1
+            except AttributeError:
+                print('該位置不存在')
+                continue
+
+            else:
+                if point is None: # 當 insert_at = 2 時候...
+                    print('該位置不存在')
+                    continue
+
+            insert_at_point(point, count, insert_at, loop_count)
+        elif select == -1:
+            make_list = False
+        else:
+            print('看不懂')
+            continue
+
+        loop_count += 1
+        do_list_traverse(head)
+        print('========================')
 
 
 
---
+++ Singly Linked List/delete.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+import sys
+from build_insert_traverse_01 import insert_at_end, Node, do_list_traverse
+
+if __name__ == '__main__':
+    count = 20
+    head = Node()
+    delete_node = True
+    delete_at = 0
+    insert_at_end(head, count, 1)
+
+    while delete_node is not False:
+        print('目前singly linked list內容：')
+        do_list_traverse(head)
+        try:
+            delete_at = int(input('刪除第幾個位置的?'))
+        except ValueError:
+            print('輸入錯誤，請重新輸入！')
+            continue
+
+        j = 1
+        point = head
+        try:
+            while j < delete_at:  # 第delete_at位置的前一個節點就可以，第delete_at位置的節點用其前一個節點的指標做指向
+                point = point.next
+                j += 1
+        except AttributeError:
+            print('該位置不存在')
+            continue
+        else:
+            if point is None:  # 當 delete_at = 2 時候...
+                print('該位置不存在')
+                continue
+
+        delete_at_node = point.next
+        point.next = point.next.next # 第delete_at位置的前一個節點的指標欄位指向第delete_at位置的後一個節點
+        print('刪除的節點內容：%s' %(delete_at_node.data))
+        print('========================')
+
